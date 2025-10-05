a6ab435855efe02b5e599b89ee3665ababb2d778
Ahmad <ahmadalhatlan@gmail.com>
2025-10-05 21:56:02 +0300

    add app-gw4

--- Azure/azurerm_app_gateway/application_gateway.tf
+++ Azure/azurerm_app_gateway/application_gateway.tf
@@ -12,8 +12,8 @@ resource "azurerm_application_gateway" "app_gateway" {
   resource_group_name = var.resource_group_name
 
   sku {
-    name     = "WAF_v2"
-    tier     = "WAF_v2"
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
     capacity = 1
   }
 
